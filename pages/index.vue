d6dc3a40413b3805904e4cafcbdffdd3258dc85a
Sharmae09 <sharmae.code@gmail.com>
2025-02-05 23:33:12 +0800

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="layout">
+    <MapboxMap />
+  </div>
+</template>
+
+<script>
+// import Map from "./components/Map.vue";
+// import "@site/src/node_modules/mapbox-gl/dist/mapbox-gl.css";
+
+export default {
+  components: {
+    Map,
+  },
+};
+</script>
+
+<style>
+#layout {
+  flex: 1;
+  display: flex;
+  position: relative;
+}
+</style>
